8dd22ed227c6d4a5ebf157d51eb02a1178e1c5a2
Celso <celopez@my.bridgeport.edu>
2022-08-21 17:50:23 +0000

    Implemented models and admin classes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['car_name', 'car_make', 'dealerId', 'car_type', 'car_year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['car_name', 'car_description']
+    inline = [CarModelInline]
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    car_name = models.CharField(null=False, max_length=20, primary_key=True)
+    car_description = models.TextField()
+
+    def __str__(self):
+        return self.car_name + " " + self.car_description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +25,26 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = "sedan"
+    SUV = "suv"
+    WAGON = "wagon"
+    RANDOM = "random car"
+
+    CAR_TYPE_CHOICES = [
+        (SEDAN,"Sedan"),
+        (SUV,"SUV"),
+        (WAGON,"Wagon"),
+        (RANDOM,"Random car"),
+    ]
+    car_make = models.ForeignKey(CarMake,null=True,on_delete=models.CASCADE)
+    car_name = models.CharField(null=False, max_length=20, primary_key=True)
+    dealerId = models.IntegerField()
+    car_type = models.CharField(null=False,choices=CAR_TYPE_CHOICES, default=RANDOM, max_length=20)
+    car_year = models.DateField(null=True)
+
+    def __str__(self):
+        return self.car_name + " " + self.car_make + " " + self.dealerId + " " + self.car_type + " " + self.car_year
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
